de3a2a6eb5d306d41593d9e8bba8224b2758992c
Gerardo <gmr.110802@gmail.com>
2021-08-26 01:44:39 -0500

    Add existing file

---
+++ src/app/tab1/tab1.module.ts
@@ -0,0 +1,48 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Tab1Page } from './tab1.page';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
+import { Tab1PageRoutingModule } from './tab1-routing.module';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    ExploreContainerComponentModule,
+    Tab1PageRoutingModule
+  ],
+  declarations: [Tab1Page]
+})
+export class Tab1PageModule {
+
+   slideOpts= {
+    initialSlide: 1,
+    speed: 400,
+    slidePerView: 2,
+    freeMode: true,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadow: true
+    }
+
+  }
+
+  slidePostOptions= {
+    initialSlide: 1,
+    speed: 400,
+    slidePerView: 2,
+    
+
+  }
+
+  constructor(){};
+}
+
+
